337b513c1ebfe35c4b5f1ee16b8216dbbc1c8085
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-12-04 12:24:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+let input = document.getElementById("evaluatedText");
+let h3 = document.querySelector("h3");
+input.addEventListener("input", onChange);
+function onChange(){
+	let str = input.value;
+	let wordCounts = countWords(str);
+	h3.innerText = wordCounts;
+}
+
+function countWords(str) {
+	str = str.trim();
+	if(str == ""){
+		return 0;
+	}
+	let word = str.split(/\s+/);
+	return word.length;
+}
